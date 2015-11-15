810a212488a1fa2ff9253dbec86be64ca21e4b93
Vernay <alexisvernay38@gmail.com>
2015-11-15 13:53:41 +0100

    Rajout d'image, modif menu, base des pages Exp et Passions faite

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,20 @@
 var gulp = require('gulp');
+    useref = require('gulp-useref'),
+    gulpif = require('gulp-if'),
+    uglify = require('gulp-uglify'),
+    minifyCss = require('gulp-minify-css');
+
+gulp.task('html', function () {
+    var assets = useref.assets();
+
+    return gulp.src('app/*.html')
+        .pipe(assets)
+        .pipe(gulpif('*.js', uglify()))
+        .pipe(gulpif('*.css', minifyCss()))
+        .pipe(assets.restore())
+        .pipe(useref())
+        .pipe(gulp.dest('dist'));
+});
 
 var sass = require('gulp-sass');
 
@@ -30,5 +46,22 @@ gulp.task('sass:watch', function () {
 
 //Ici la tache par default
 gulp.task('default', function() {
-  // place code for your default task here
+
+});
+
+gulp.task('build', ['buildwww', 'buildImg']);
+
+gulp.task('buildwww', function() {
+  var assets = useref.assets();
+
+  return gulp.src('./src/*.html')
+        .pipe(assets)
+        .pipe(assets.restore())
+        .pipe(useref())
+        .pipe(gulp.dest('www'));
+});
+
+gulp.task('buildImg', function() {
+  return gulp.src('./src/images/**/*')
+        .pipe(gulp.dest('build'));
 });
